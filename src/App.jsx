586a2764cc2579b358316ba619d1b2cf44688757
Venkatesh <sahoovenketeswar@gmail.com>
2023-10-20 22:28:08 +0530

    React Progress Circle is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,62 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useEffect, useRef } from "react";
 
-const PasswordInput = () => {
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [allRequirementsMet, setAllRequirementsMet] = useState(false);
+export default function ProgressCircle() {
+  const scrollToTopElement = useRef(null);
 
-  const requirements = useMemo(
-    () => [
-      { regex: /.{8,}/, text: "At least 8 characters length" },
-      { regex: /[0-9]/, text: "At least 1 number (0...9)" },
-      { regex: /[a-z]/, text: "At least 1 lowercase letter (a...z)" },
-      { regex: /[^A-Za-z0-9]/, text: "At least 1 special symbol (!...$)" },
-      { regex: /[A-Z]/, text: "At least 1 uppercase letter (A...Z)" }
-    ],
-    []
-  );
+  const updateProgressCircle = () => {
+    const progressElement = document.querySelector(".progress-circle-bar");
+    const scrollToTopElementRef = scrollToTopElement.current;
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    let progress = window.pageYOffset / totalHeight * 283;
+    progress = Math.min(progress, 283);
+    progressElement.style.strokeDashoffset = 283 - progress;
 
-  const handlePasswordChange = e => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+      scrollToTopElementRef.style.opacity = "1";
+    } else {
+      scrollToTopElementRef.style.opacity = "0";
+    }
   };
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  useEffect(
-    () => {
-      const allMet = requirements.every(req => req.regex.test(password));
-      setAllRequirementsMet(allMet);
-    },
-    [password, requirements]
-  );
+  useEffect(() => {
+    updateProgressCircle();
+    window.addEventListener("scroll", updateProgressCircle);
+    window.addEventListener("resize", updateProgressCircle);
+
+    return () => {
+      window.removeEventListener("scroll", updateProgressCircle);
+      window.removeEventListener("resize", updateProgressCircle);
+    };
+  }, []);
 
   return (
-    <div className="wrapper">
-      <div className="pass-field">
-        <input
-          type={showPassword ? "text" : "password"}
-          placeholder="Create password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <i
-          className={`fa-solid fa-eye${showPassword ? "-slash" : ""}`}
-          onClick={togglePasswordVisibility}
-        />
-      </div>
-      <div className="content">
-        <p>Password must contain:</p>
-        <ul className="requirement-list">
-          {requirements.map((req, index) =>
-            <li key={index} className={req.regex.test(password) ? "valid" : ""}>
-              <i
-                className={`fa-solid ${req.regex.test(password)
-                  ? "fa-check"
-                  : "fa-circle"}`}
-              />
-              <span>
-                {req.text}
-              </span>
-            </li>
-          )}
-        </ul>
-      </div>
-      <div className="wrapper">
-        <button disabled={!allRequirementsMet}>Submit</button>
+    <div className="progress-circle-container">
+      <div />
+      <svg className="progress-circle" viewBox="0 0 100 100">
+        <circle className="progress-background" cx={50} cy={50} r={45} />
+        <circle className="progress-circle-bar" cx={50} cy={50} r={45} />
+      </svg>
+      <div
+        className="scroll-to-top"
+        onClick={scrollToTop}
+        ref={scrollToTopElement}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M12 19V5M5 12l7-7 7 7" />
+        </svg>
       </div>
     </div>
   );
-};
-
-export default PasswordInput;
+}
